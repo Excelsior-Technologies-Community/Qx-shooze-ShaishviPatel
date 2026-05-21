07580aa5eb66ddae7b3b1d8eac47392e054d7f49
Shaishvi Patel <shaishvipatel125@gmail.com>
2026-05-21 14:36:33 +0530

    21_05_26_footer_commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './Components/Header'
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+// import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Header />
 
+      {/* <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main> */}
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -217,6 +217,7 @@ const Header = () => {
                 </div>
               </div>
             </Dialog>
+            
             {/*forgot box dailogbox*/}
             <Dialog
               open={open && view === "forgot"}
