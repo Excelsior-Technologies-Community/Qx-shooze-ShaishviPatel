e4cf5b91f6b106b9b991b379e10bb62282f596da
Shaishvi Patel <shaishvipatel125@gmail.com>
2026-05-26 12:24:05 +0530

    productflip_commit

--- src/data/productData.js
+++ src/data/productData.js
@@ -1,12 +1,11 @@
 
-export const products = [
+export const featuredProducts = [
   {
     id: 1,
     name: "Classic White Tennis Sneakers",
     price: 25.00,
     brand: "SportyFeet",
-    categories: ["featured", "new arrivals"], 
-    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-3_be4a38ab-621f-46d5-a126-3488687e10f6.jpg?v=1731311211&width=1200",
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-4_a9f5532a-47cd-4f32-8958-57ee765f0a27.jpg?v=1731311278&width=1200",
     img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-19_323f61e6-cfd6-4407-823a-273f154d188c.jpg?v=1731311295&width=360"
   },
   {
@@ -14,8 +13,7 @@ export const products = [
     name: "Waterproof Hiking Boots",
     price: 35.00,
     brand: "TrailGear",
-    categories: ["featured"],
-    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-4_a9f5532a-47cd-4f32-8958-57ee765f0a27.jpg?v=1731311278&width=1200",
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-17.jpg?v=1731315215&width=1200",
     img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-23_8eeee338-7bad-4c2b-b296-6804d886a41a.jpg?v=1731315325&width=360"
   },
   {
@@ -23,17 +21,87 @@ export const products = [
     name: "Classic Leather Sneakers",
     price: 21.00,
     brand: "UrbanStep",
-    categories: ["new arrivals", "best seller"],
-    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-17.jpg?v=1731315215&width=1200",
-    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-26_a72ab182-4323-4754-aa41-d64401571e17.jpg?v=1731311225&width=360"
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-3_be4a38ab-621f-46d5-a126-3488687e10f6.jpg?v=1731311211&width=1200",
+    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-26_a72ab182-4323-4754-aa41-d64401571e17.jpg?v=1731311225&width=720"
   },
   {
     id: 4,
     name: "High-Top Canvas Sneakers",
     price: 25.00,
     brand: "TrendyFeet",
-    categories: ["best seller"],
     img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-7_bf9fee80-650d-4775-a219-0eaccf66d47b.jpg?v=1731311538&width=1200",
-    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-19_49ae4265-2610-48a8-b934-1a24b6136832.jpg?v=1731311550&width=360"
+    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-19_49ae4265-2610-48a8-b934-1a24b6136832.jpg?v=1731311550&width=720",
+  },
+];
+
+export const newArrivalProducts = [
+  {
+    id: 5,
+    name: "Street Runner Low Tops",
+    price: 28.00,
+    brand: "NovaStep",
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-7_bf9fee80-650d-4775-a219-0eaccf66d47b.jpg?v=1731311538&width=1200",
+    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-23_8eeee338-7bad-4c2b-b296-6804d886a41a.jpg?v=1731315325&width=360"
+  },
+  {
+    id: 6,
+    name: "All-Terrain Walkers",
+    price: 38.00,
+    brand: "TrailGear",
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-3_be4a38ab-621f-46d5-a126-3488687e10f6.jpg?v=1731311211&width=1200",
+    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-19_49ae4265-2610-48a8-b934-1a24b6136832.jpg?v=1731311550&width=720"
+  },
+  {
+    id: 7,
+    name: "Retro Court Sneakers",
+    price: 22.00,
+    brand: "UrbanStep",
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-4_a9f5532a-47cd-4f32-8958-57ee765f0a27.jpg?v=1731311278&width=1200",
+    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-26_a72ab182-4323-4754-aa41-d64401571e17.jpg?v=1731311225&width=720"
+  },
+  {
+    id: 8,
+    name: "Canvas Street High Tops",
+    price: 26.00,
+    brand: "TrendyFeet",
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-17.jpg?v=1731315215&width=1200",
+    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-19_323f61e6-cfd6-4407-823a-273f154d188c.jpg?v=1731311295&width=360",
+  },
+];
+
+export const bestSellerProducts = [
+  {
+    id: 9,
+    name: "Weekend Classic Trainers",
+    price: 29.00,
+    brand: "SportyFeet",
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-3_be4a38ab-621f-46d5-a126-3488687e10f6.jpg?v=1731311211&width=1200",
+    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-23_8eeee338-7bad-4c2b-b296-6804d886a41a.jpg?v=1731315325&width=360"
+  },
+  {
+    id: 10,
+    name: "Trail Peak Boots",
+    price: 42.00,
+    brand: "TrailGear",
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-7_bf9fee80-650d-4775-a219-0eaccf66d47b.jpg?v=1731311538&width=1200",
+    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-19_49ae4265-2610-48a8-b934-1a24b6136832.jpg?v=1731311550&width=720"
+  },
+  {
+    id: 11,
+    name: "Signature Leather Kicks",
+    price: 24.00,
+    brand: "UrbanStep",
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-17.jpg?v=1731315215&width=1200",
+    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-26_a72ab182-4323-4754-aa41-d64401571e17.jpg?v=1731311225&width=720"
+  },
+  {
+    id: 12,
+    name: "City Canvas High Tops",
+    price: 27.00,
+    brand: "TrendyFeet",
+    img1: "//qx-shooz.myshopify.com/cdn/shop/files/product-4_a9f5532a-47cd-4f32-8958-57ee765f0a27.jpg?v=1731311278&width=1200",
+    img2: "https://qx-shooz.myshopify.com/cdn/shop/files/product-19_323f61e6-cfd6-4407-823a-273f154d188c.jpg?v=1731311295&width=360",
   }
 ];
+
+export const products = [...featuredProducts, ...newArrivalProducts, ...bestSellerProducts];
